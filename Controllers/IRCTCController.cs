914f22b3da23f79339c33f6f43cbf8c9028a47c0
lenovo <lenovo@DESKTOP-RVVGKAI>
2022-04-30 12:32:35 +0530

    Add project files.

---
+++ Controllers/IRCTCController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNET_Project1.Controllers
+{
+    public class IRCTCController : Controller
+    {
+        // GET: IRCTC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Trains()
+        {
+            return View();
+        }
+        public ActionResult Buses()
+        {
+            return View();
+        }
+        public ActionResult Flights()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNET_Project1.MvcApplication" Language="C#" %>
